5d1474574a68faec5bec80353eaf358612a95ffd
sdress <sarah.dresser1@gmail.com>
2022-03-12 18:16:30 -0800

    adding completed project files

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 from flask_app import app
-from flask_app.controllers import authors
-from flask_app.controllers import books
+from flask_app.controllers import authors, books
 
 if __name__ == "__main__":
     app.run(debug=True)
